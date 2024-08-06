cd418d84442144882c87131ba161a85f0e43de60
x <chrisseiler96@gmail.com>
2024-08-05 22:09:21 -0400

    hero2

--- src/lib/components/HeroImage.svelte
+++ src/lib/components/HeroImage.svelte
@@ -26,7 +26,7 @@
         blur-[120px] filter
         "
         />
-        <img class="rounded-lg" src='/assets/hero.gif' alt="hero"/>	
+        <img class="rounded-lg" src='/assets/hero2.gif' alt="hero"/>	
     </div>
 
 <style>
