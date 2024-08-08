977fdfb80438c1c96efdc4b3e69daccba26272c4
x <chrisseiler96@gmail.com>
2024-08-08 09:39:05 -0400

    prism

--- src/App.svelte
+++ src/App.svelte
@@ -75,7 +75,7 @@
   </nav>
 
   <Bounded>
-    <HeroText hook={"Next-Gen AI for Sales and Support"} body={'Use our assistant Eta to create personalized shopping experiences for your website.'}></HeroText>
+    <HeroText hook={"Next-Gen AI for Sales and Support"} body={'Use Prism to create personalized shopping experiences for your website.'}></HeroText>
     
     <div class="cta-container flex justify-center items-center w-full mb-12">
       <CTA
